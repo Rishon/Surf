d854e2e8f698a0e9749fa0f0fcad8a10d8f37cd2
Rishon Jaffe <rishonjaffe9@gmail.com>
2026-01-12 23:45:24 +0200

    Add shutdown hook

--- src/main/java/systems/rishon/surf/SurfProxy.java
+++ src/main/java/systems/rishon/surf/SurfProxy.java
@@ -20,6 +20,8 @@ public class SurfProxy {
     private final ProxyConfig config;
     private final JoinServerRouter joinRouter;
 
+    private QuicProxyServer server;
+
     public SurfProxy(ProxyConfig config) {
         this.config = config;
         this.joinRouter = new JoinServerRouter(config);
@@ -33,12 +35,21 @@ public class SurfProxy {
             ProxyConfig config = ProxyConfigLoader.load(inputStream);
             SurfProxy proxy = new SurfProxy(config);
             proxy.start();
+
+            proxy.listenForShutdown(proxy.server);
         }
     }
 
-    public void start() {
-        QuicProxyServer server = new QuicProxyServer(config, this);
-        server.start();
+    private void start() {
+        this.server = new QuicProxyServer(config, this);
+        this.server.start();
+    }
+
+    private void listenForShutdown(QuicProxyServer server) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            this.logger.warning("SurfProxy shutting down");
+            server.shutdown();
+        }));
     }
 
     public void onPlayerConnect(
@@ -50,7 +61,7 @@ public class SurfProxy {
         BackendServer target = joinRouter.selectInitialServer();
         session.connectTo(target);
 
-        logger.info("Session " + session.sessionId()
+        this.logger.info("Session " + session.sessionId()
                 + " routed to join server "
                 + target.name()
                 + " (" + target.host() + ":" + target.port() + ")");
